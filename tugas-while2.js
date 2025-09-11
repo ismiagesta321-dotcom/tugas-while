0021ec6574dd7d96db56de1feecac3c793976be2
ismiagesta321-dotcom <ismiagesta321@gmail.com>
2025-09-11 09:08:35 +0700

    done

---
+++ tugas-while2.js
@@ -0,0 +1,11 @@
+var nilai=0;
+while (nilai>=0 && nilai<=14){
+    console.log(nilai + ' = genap');
+    nilai+=2;
+}
+
+var nilai2=1;
+while (nilai2>=1 && nilai2 <=15){
+    console.log(nilai2 + ' = ganjil');
+    nilai2+=2;
+}
